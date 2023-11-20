d5a8d43b1cda5584b685fdc88dd97c0e798b4f7e
Nico <nicolas439@gmail.com>
2023-11-20 15:43:34 +0100

    SRP

--- Single Responsibility Principle (SRP)/bad code/ToDoList.py
+++ Single Responsibility Principle (SRP)/bad code/ToDoList.py
@@ -1,2 +1,22 @@
 class ToDoList:
-    def
+    def init (self):
+        self.tasks = []
+
+    def add_task (self, task):
+        self.tasks.append(task)
+
+    def delete_task (self, task):
+        self.task.remove(task)
+    
+    def display_task (self):
+        for task in self.tasks:
+            print (task)
+
+    def input_task (self):
+        task = input("Enter a task name: ")
+        self.add_task(task)
+
+    def remove_task (self):
+        task = input ("Enter the same of the task to remove: ")
+        self.delete_task(task)
+
